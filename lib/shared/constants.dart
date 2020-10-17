3fa2d4c8ee7fcad69473e318de4f0d3ad6a6c0f3
RobertKrzywina <robert.krzywina@gmail.com>
2020-10-17 23:15:38 +0200

    Change logo and title of app

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -6,7 +6,7 @@ const String SERVER_IP = 'http://10.0.2.2:8080/api';
 const String ANDROID_APP_ID = 'pl.givejob.givejobtimer_mobile';
 const String IOS_APP_ID = null;
 
-const String APP_NAME = 'GiveJob Timer';
+const String APP_NAME = 'GiveJobTimer';
 
 const String ROLE_EMPLOYEE = 'ROLE_EMPLOYEE';
 const String ROLE_MANAGER = 'ROLE_MANAGER';
